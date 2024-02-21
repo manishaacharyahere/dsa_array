5a63c4423196891c4713404031308f785f12796a
manishaacharyahere <154820530+manishaacharyahere@users.noreply.github.com>
2024-02-22 01:10:44 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,33 @@
+import java.io.*;
+/**
+ * reverse
+ */
+public class reverse {
+
+    static void reverseArray(int arr[],int start,int end){
+        int temp;
+        while(start<end){
+            temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+    static void printArray(int arr[], int size) 
+    { 
+        for (int i = 0; i < size; i++) 
+            System.out.print(arr[i] + " "); 
+         
+        System.out.println(); 
+    }
+    public static void main(String args[]) { 
+         
+        int arr[] = {1, 2, 3, 4, 5, 6,7}; 
+        printArray(arr, 7); 
+        reverseArray(arr, 0, 6); 
+        System.out.print("Reversed array is \n"); 
+        printArray(arr, 7); 
+         
+    }
+}
